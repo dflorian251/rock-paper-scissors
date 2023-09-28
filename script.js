e9d1554b99e16df50feda659941e8f81df544c9d
dflorian251 <inf2021044@ionio.gr>
2023-09-28 17:56:39 +0300

    Draw condition fixed
    The last else statement moved to each nested loop.
    The previous one was a check for invalid values, but not a check for draws.

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ function getComputerChoice(){
     return computerChoice ; 
 }
 
-
-let computerSelection = getComputerChoice();
-
 function round(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase() ;
     
@@ -22,21 +19,26 @@ function round(playerSelection, computerSelection){
             console.log("You lose! Scissors beat paper.");
         }else if (computerSelection == "rock") {
             console.log("You win! Paper wins rock.");
+        }else { 
+            console.log("Draw!")
         }
     }else if (playerSelection == "rock"){ // Player selects rock
         if (computerSelection == "paper") {
             console.log("You lose ! Paper wins rock.");
         } else if (computerSelection == "scissors") {
             console.log("You win! Rock beats scissors!")
+        }else { 
+            console.log("Draw!")
         }
     }else if (playerSelection == "scissors") { // Player selects scissors
         if (computerSelection == "rock") {
             console.log("You lose! Rock beats scissors.")
         }else if (computerSelection == "paper") {
             console.log("You win! Scissors beats paper.")
+        }else { 
+            console.log("Draw!")
         }
-    } else { //Player and computer select the same
-        console.log("Draw!")
     }
 }
 
+round("scissors", getComputerChoice())
